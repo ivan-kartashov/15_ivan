fccaaedc2a1ddb598ae4ad53b18919eba461164b
ivan-kartasov <ivan.kartashov1650@gmail.com>
2025-12-05 13:48:25 +0100

    javascript basico

---
+++ 4/pruebajavascript.js
@@ -0,0 +1,16 @@
+// Soy un comentario en javascript
+/* Comentario
+Multilinea
+*/
+//alert('prueba de código')
+var mensaje = "hola mundo qué tal";
+console.log(mensaje);
+console.log(mensaje);
+console.log(document);
+console.log(localStorage);
+var esViernes = true;
+console.log('Es viernes y queremos irnos:' + esViernes);
+//debugger para abrir el depurador del codigo en esta linea
+if(esViernes == true) {
+    console.log('Pues sí que es viernes, nos quedan 45 minutos');
+}
