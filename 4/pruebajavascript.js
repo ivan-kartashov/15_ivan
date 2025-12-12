c2be3414b414bda701e5ce223bdf09e3e053fd96
ivan-kartasov <ivan.kartashov1650@gmail.com>
2025-12-12 14:03:30 +0100

    Javascript

--- 4/pruebajavascript.js
+++ 4/pruebajavascript.js
@@ -14,6 +14,35 @@ console.log('Es viernes y queremos irnos:' + esViernes);
 if(esViernes == true) {
     console.log('Pues sí que es viernes, nos quedan 45 minutos');
 }
-var nombreUsuario = prompt('Introduzca su nombre')
-console.log('El nombre del usuario es: ' + nombreUsuario)
-document.write("<h1>" + nombreUsuario + "</h1>");
+//var nombreUsuario = prompt('Introduzca su nombre')
+
+function enviaFormulario() {
+    console.log("Llamando a la función enviaFormulario");
+    var div = document.getElementById("mensajes");
+    //Las pass coinciden
+    if(comprobarPass()) {
+        //console.log('El nombre del usuario es: ' + nombre);
+        //document.writeln("<h1>" + nombre + "</h1>");
+        //document.writeln("<h1>Password OK</h1>");
+        var nombre = document.getElementById("nombre").value;
+        div.innerHTML = nombre + " Password OK";
+        div.style.backgroundColor = "green";
+    } else {
+        //document.writeln("<h1>Password not okay</h1>")
+        var nombre = document.getElementById("nombre").value;
+        div.innerHTML = nombre + " Password KO";
+        div.style.backgroundColor = "red";
+    }
+
+} 
+
+function comprobarPass() {
+    var pass1 = document.getElementById("pass1").value;
+    var pass2 = document.getElementById("pass2").value;
+    return pass1 == pass2
+}
+
+function cambiaColor(color) {
+    var div = document.getElementById("mensajes");
+    div.style.backgroundColor = color;
+}
